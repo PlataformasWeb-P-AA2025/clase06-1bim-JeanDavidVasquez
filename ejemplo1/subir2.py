5a15d071c8066253a8a0d89c28bc55d12f368daf
¨jeandavidvasquez¨ <jeandavidvasquezyaruqui@gmail.com>
2025-05-14 09:06:16 -0500

    cambios Jean2

--- ejemplo1/subir2.py
+++ ejemplo1/subir2.py
@@ -1,40 +1,14 @@
 import csv
 from sqlalchemy.orm import sessionmaker
-
-from crear_base import Saludo
+from crear_base import Saludo2
 from configuracion import engine
-
-csv_file = '/home/jeanproject/Documentos/Semana6/clase06-1bim-JeanDavidVasquez/ejemplo1/data/saludos_mundo.csv'
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# se crea un objeto de tipo
-# Saludo
-
-with open(csv_file, newline='', encoding='latin-1') as f:
-    #convierto cada fila del CSV en un diccionario, donde las claves son los nombres de las columnas.
-    reader = csv.DictReader(f)
-    #creo una lista con solo las filas que no estén completamente vacías.
-    documentos = [row for row in reader if any(row.values())]  # Ignorar filas vacías
-
-
-miSaludo = Saludo()
-miSaludo.mensaje = "Hola que tal"
-miSaludo.tipo = "informal"
-
-miSaludo2 = Saludo()
-miSaludo2.mensaje = "Buenas tardes"
-miSaludo2.tipo = "formal"
-
-
-# se agrega el objeto miSaludo
-# a la entidad Saludo a la sesión
-# a la espera de un commit
-# para agregar un registro a la base de
-# datos demobase.db
-session.add(miSaludo)
-session.add(miSaludo2)
-
-# se confirma las transacciones
+with open('data/saludos_mundo.csv', encoding='latin-1') as f:
+    reader = csv.reader(f, delimiter='|')
+    next(reader)
+    for row in reader:
+        session.add(Saludo2(mensaje=row[0].strip(), tipo=row[1].strip(), origen=row[2].strip()))
 session.commit()
